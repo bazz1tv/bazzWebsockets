839f2c05512b507bf332183a952a79d3b8f7c211
Bazz <bazz@bazz1.com>
2021-08-12 00:22:26 -0400

    add Transfer model

---
+++ models/transfer.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Transfer extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Transfer.init({
+    from: DataTypes.STRING,
+    to: DataTypes.STRING,
+    amount: DataTypes.INTEGER,
+    message: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Transfer',
+  });
+  return Transfer;
+};
